fcf9da11df970bc3832b3b1586f0d33fcd2948fa
Oleg Shevchuk <sh.oleg@list.ru>
2023-01-18 16:46:01 +0600

    Add ForwardMessages method

--- src/tools/Sending.php
+++ src/tools/Sending.php
@@ -373,4 +373,29 @@ class Sending {
 		return $this->greenApi->request( 'POST',
 			'{{host}}/waInstance{{idInstance}}/SendTemplateButtons/{{apiTokenInstance}}', $requestBody );
 	}
+
+	/**
+	 * The method is intended for forwarding messages to a personal or group chat. The forwarded messages will be added
+	 * to the send queue. Checking whatsapp authorization on the phone (i.e. availability in linked devices) is not performed.
+	 * The message will be kept for 24 hours in the queue and will be sent immediately after phone authorization.
+	 * The rate at which messages are sent from the queue is managed by Message sending delay parameter.
+	 *
+	 * @param string $chatId
+	 * @param string $chatIdFrom
+	 * @param array $messages
+	 *
+	 * @return stdClass
+	 * @link https://green-api.com/en/docs/api/sending/ForwardMessages/
+	 */
+	public function forwardMessages( string $chatId, string $chatIdFrom, array $messages ): stdClass {
+
+		$requestBody = [
+			'chatId' => $chatId,
+			'chatIdFrom' => $chatIdFrom,
+			'messages' => $messages
+		];
+
+		return $this->greenApi->request( 'POST',
+			'{{host}}/waInstance{{idInstance}}/ForwardMessages/{{apiTokenInstance}}', $requestBody );
+	}
 }
