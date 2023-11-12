a19a2776e74430602fe26e1e05b5ef7e3cf4c62b
Jibek100 <zhibek.tazhdavletova@nu.edu.kz>
2023-11-12 18:13:33 +0600

    SW-2544: Added sendPoll request and example

--- examples/sendPoll.php
+++ examples/sendPoll.php
@@ -8,7 +8,7 @@ define( "API_TOKEN_INSTANCE", "d75b3a66374942c5b3c019c698abc2067e151558acbd41234
 
 $greenApi = new GreenApiClient(ID_INSTANCE, API_TOKEN_INSTANCE);
 
-$result = $greenApi->sending->sendPoll('11001234567@c.us', 'Message text',
+$result = $greenApi->sending->sendPoll('11001234567@c.us', 'Please choose the color:',
     array(array('optionName'=>'green'), array('optionName'=>'red'), array('optionName'=>'blue')));
 
 print_r($result->data);
